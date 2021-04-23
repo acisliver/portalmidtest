59d92d6f51672d2f77964e7adf0a28fdb13628ce
acisliver <7273322@naver.com>
2021-04-23 17:13:58 +0900

    Template CallbackPattern sql, params 분리

--- userdao/src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -12,24 +12,31 @@ public class UserDao {
 
 
     public User get(Integer id) throws SQLException {
+        String sql = "select * from userinfo where id = ?";
+        Object[] params = new Object[]{id};
         return context.JdbcContextForGet(connection -> {
             PreparedStatement preparedStatement = connection.prepareStatement(
-                    "select * from userinfo where id = ?"
+                    sql
             );
-            preparedStatement.setLong(1, id);
+            for(int i = 0; i < params.length; i++){
+                preparedStatement.setObject(i + 1, params[i]);
+            }
             return preparedStatement;
         });
     }
 
 
     public void insert(User user) throws SQLException{
+        String sql = "insert into userinfo (name, password) values (?, ?)";
+        Object[] params = new Object[]{user.getName(), user.getPassword()};
         context.JdbcContextForInsert(user, connection -> {
             PreparedStatement preparedStatement = connection.prepareStatement(
-                    "insert into userinfo (name, password) values (?, ?)",
+                    sql,
                     Statement.RETURN_GENERATED_KEYS
             );
-            preparedStatement.setString(1, user.getName());
-            preparedStatement.setString(2, user.getPassword());
+            for(int i = 0; i < params.length; i++){
+                preparedStatement.setObject(i + 1, params[i]);
+            }
             return preparedStatement;
         });
 
@@ -37,27 +44,33 @@ public class UserDao {
 
 
     public void update(User user) throws SQLException {
+        String sql = "update userinfo set name = ?, password = ? where id = ?";
+        Object[] params = new Object[]{user.getName(), user.getPassword(), user.getId()};
 
         context.JdbcContextForUpdate(connection -> {
             PreparedStatement preparedStatement = connection.prepareStatement(
-                    "update userinfo set name = ?, password = ? where id = ?",
+                    sql,
                     Statement.RETURN_GENERATED_KEYS
             );
-            preparedStatement.setString(1, user.getName());
-            preparedStatement.setString(2, user.getPassword());
-            preparedStatement.setInt(3, user.getId());
+            for(int i = 0; i < params.length; i++){
+                preparedStatement.setObject(i + 1, params[i]);
+            }
             return preparedStatement;
         });
     }
 
 
     public void delete(Integer id) throws SQLException {
+        Object[] params = new Object[]{id};
+
         context.JdbcContextForDelete(connection -> {
             PreparedStatement preparedStatement = connection.prepareStatement(
                     "delete from userinfo where id = ?",
                     Statement.RETURN_GENERATED_KEYS
             );
-            preparedStatement.setInt(1, id);
+            for(int i = 0; i < params.length; i++){
+                preparedStatement.setObject(i + 1, params[i]);
+            }
             return preparedStatement;
         });
     }
